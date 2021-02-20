ebdd9cdb5b7dd3d9051797071e6f68336dfc7657
yoshist <stacy.yoshimitsu@ngc.com>
2021-02-20 21:50:17 +0000

    chef day2

--- cookbooks/test_cookbook/recipes/default.rb
+++ cookbooks/test_cookbook/recipes/default.rb
@@ -9,7 +9,10 @@ execute "update-upgrade" do
   command "sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' upgrade"
 
   action :run
-
 end
+file '/tmp/myfile.txt' do
+  content 'Hello'
+end
+
 
 
--- cookbooks/test_cookbook/spec/spec_helper.rb
+++ cookbooks/test_cookbook/spec/spec_helper.rb
@@ -1,2 +1,9 @@
 require 'chefspec'
-require 'chefspec/policyfile'
+describe 'test_cookbook::default' do 
+  let(:chef_run) {  
+    ChefSpec::Runner.new.converge(described_recipe)  
+  } 
+   it 'creates a file' do 
+   expect(chef_run).to create_file('/tmp/myfile.txt')  
+  end 
+end
